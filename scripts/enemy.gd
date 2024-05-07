fa6d138362ca470a71a9a79ef0462c2b71ed1268
Vanessa <vanessa.araujoprogramacao@gmail.com>
2024-05-07 15:15:23 -0300

    debug do voou do inimigo_1

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,46 +1,38 @@
 extends CharacterBody2D
 
 
-const SPEED = 800.0
-const FLY_FORCE = 100.0  # Força para voar
+const SPEED = 600.0
+const JUMP_VELOCITY = -400.0
 
+@onready var wall_detector := $RayCast2D as RayCast2D
 @onready var texture := $texture as Sprite2D
-@onready var anim:= $anim as AnimationPlayer
+@onready var anim := $anim as AnimationPlayer
 
 var direction := -1
-var is_flying := true
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+func _physics_process(delta: float) -> void:    
 
-	velocity.y = -FLY_FORCE * delta
+	if is_on_floor():
+		velocity.y += gravity * delta * delta
+
+	if wall_detector.is_colliding():
+		direction *= -1
+		wall_detector.scale.x *= -1
+
+	if direction ==1:
+		texture.flip_h = true
+	else:
+		texture.flip_h = false
+
 	velocity.x = direction * SPEED * delta
 
-		
-		
 	move_and_slide()
-
-
-func _ready() -> void:
-	set_physics_process(true)
-
-
-func _fixed_process(delta: float) -> void:
-	# Change direction when reaching boundaries
-	if position.x <= 100:
-		direction = 1
-	elif position.x >= 700:
-		direction = -1
 
 
 func _on_anim_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "hurt":
 		Globals.score += 100
 		queue_free()
-		Globals.score += 100
--- scripts/world_01.gd
+++ scripts/world_01.gd
@@ -6,7 +6,6 @@ extends Node2D
 func _ready():
 	player.follow_camera(camera)
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
