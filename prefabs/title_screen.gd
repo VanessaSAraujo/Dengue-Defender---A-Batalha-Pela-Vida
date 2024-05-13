9dab803fea1856f139f7bb68ef0385c129052fa7
Vanessa <vanessa.araujoprogramacao@gmail.com>
2024-05-12 21:13:33 -0300

    Tela de GameOver

--- prefabs/title_screen.gd
+++ prefabs/title_screen.gd
@@ -4,8 +4,8 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
 	start_btn.grab_focus()
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- scripts/world_01.gd
+++ scripts/world_01.gd
@@ -8,7 +8,7 @@ extends Node2D
 func _ready():
 	player.follow_camera(camera)
 	player.player_has_died.connect(reload_game)
-	control.time_is_up.connect(reload_game)
+	control.time_is_up.connect(game_over)
 	Globals.coins = 0
 	Globals.score = 0
 	Globals.player_life = 3
@@ -20,3 +20,6 @@ func _process(delta):
 func reload_game():
 	await get_tree().create_timer(1.0).timeout
 	get_tree().reload_current_scene()
+
+func game_over():
+	get_tree().change_scene_to_file("res://prefabs/game_over.tscn")
